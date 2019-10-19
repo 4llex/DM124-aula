fe87c6a04dad989b0154015c596fd2713b63c01c
Alex Rafael Silva Rosa <alexr@gec.inatel.br>
2019-10-19 16:54:23 -0300

    promise 2

--- 02-file-system/promise.js
+++ 02-file-system/promise.js
@@ -1,12 +1,18 @@
 function bakeACake() {
     console.log('Start baking the bake')
     return new Promise((resolve, reject) => {
-        setTimeout(() => resolve('the cake is ready!!'), 5000);
-        reject('no cake today');
+        setTimeout(() => resolve('the cake is ready!!'), 1000);
+        //reject('no cake today');
     });
 }
 
+function addFrosting(cake) {
+    return new Promise((resolve,reject) => {
+        resolve(cake + 'with a delicious frosting!');
+    })
+}
 
 bakeACake()
+    .then(addFrosting)
     .then(data => console.log(data))
     .catch(error => console.log(error));
