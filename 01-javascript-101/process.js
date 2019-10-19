0437544729de67febf058f1f827873dbdb407360
Alex Rafael Silva Rosa <alexr@gec.inatel.br>
2019-10-19 14:32:51 -0300

    file different printers

--- 01-javascript-101/process.js
+++ 01-javascript-101/process.js
@@ -1,2 +1,12 @@
-console.log(process.argv);
-console.log(process.argv.slice(2));
+const names = process.argv.slice(2);
+
+function regularPrinter(name){
+    console.log(name);
+}
+
+function fancyPrinter(name){
+    console.log(`<--- ${name} ----->`)
+}
+
+const print = fancyPrinter;
+names.forEach(print);
